73fc82e5e4a0f50543ac4ea9c83a96886d4b7693
keiwv <141773283+keiwv@users.noreply.github.com>
2023-09-17 18:53:31 -0700

    modified:   Practica_2/Practica2.py

---
+++ Practica_2/Practica2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+#Creating a vector
+vector = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+#Display the value of the vector using a loop
+print("Mostrando los datos guardados en un vector: ")
+for value in vector:
+    print(value)
+
+#Using the line "lenght" you can know the length of the vector
+lenght = len(vector)
+print("Longitud del vector:", lenght)
